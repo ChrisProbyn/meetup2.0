65e9605528f6d3d3c122a116a5570e0c2301e3fe
Brian <wubrian1234@hotmail.com>
2019-01-25 11:46:19 -0800

    initialize expo

---
+++ App.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
+import Marker from 'react-native-maps';
+
+export default class App extends React.Component {
+  render() {
+    return (
+<MapView
+     provider={PROVIDER_GOOGLE}
+     style={{flex: 1}}
+     region={{
+       latitude: 49.281372,
+       longitude: -123.114542,
+       latitudeDelta: 0.0922,
+       longitudeDelta: 0.0421
+     }}
+     showsUserLocation={true}
+     followsUserLocation = {true}
+   >
+     <MapView.Marker
+           coordinate={{latitude: 49.2834,
+           longitude: -123.1164}}
+           title={"The Keg"}
+           description={"Steak"}
+        />
+   </MapView>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
