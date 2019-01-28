606446b9e51ee436c65d1aedea3423724780ff39
Chris Probyn <cprobyn16@gmail.com>
2019-01-27 16:56:29 -0800

    created landing page with login/signup

--- client/App.js
+++ client/App.js
@@ -2,13 +2,67 @@ import React from 'react';
 import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import Map from './components/map components/Map.js';
 import Chat from './components/chat components/chat.js';
+import Landing from './components/landing components/Landing.js';
 
 export default class App extends React.Component {
   constructor() {
     super()
+    
     this.state = {
-      viewSection :false
+      viewSection :false,
+      backgroundImage: require("./assets/background.png"),
+      currentUserID: null,
+      users: [
+        {
+          id: 1,
+          email: "Example1@gmail.com",
+          password: "123",
+          image: "",
+        },
+        {
+          id: 2,
+          email: "example2@gmail.com",
+          password: "456",
+          image: "",
+        },
+        {
+          id: 3,
+          email: "example3@gmail.com",
+          password: "456",
+          image: "",
+        },
+      ],
+      groups: [
+        {
+          id: 1,
+          group_name: "group 1",
+          users_id: [1,2]
+        },
+        {
+          id: 2,
+          group_name: "group 2",
+          users_id: [2,3]
+        }
+      ],
+      messages: [
+        {
+        group_id: 1,
+        user_id: 1,
+        content: "This is user 1 typing!"
+      },
+      {
+        group_id: 1,
+        user_id: 2,
+        content: "This is user 2 typing!"
+      },
+      {
+        group_id: 2,
+        user_id: 3,
+        content: "This is user 3 typing!"
+      }] 
     }
+    this.changeUser = this.changeUser.bind(this);
+    this.addUser = this.addUser.bind(this);
   }
 
   renderBottomComponent() {
@@ -23,6 +77,21 @@ buttonPress=()=>{
     this.setState({viewSection:true})
 }
 
+changeUser = incomingUser =>{
+  this.setState({
+    currentUserID: incomingUser
+  })
+}
+addUser =incomingUser =>{
+  const oldUsers = this.state.users;
+  const newUsers = [...oldUsers, incomingUser];
+  this.setState({ users: newUsers});
+  //then
+  // this.setState({
+  //   currentUserID: incomingUserID
+  // })
+
+ }
   render() {
     return (
       <React.Fragment>
@@ -32,10 +101,8 @@ buttonPress=()=>{
       </TouchableOpacity>
       </View> */}
 
-      <Chat />
-
-      {this.renderBottomComponent()}
-
+      <Landing background={this.state.backgroundImage} users={this.state.users} changeUser={this.changeUser} addUser={this.addUser}/>
+      {/* <View><Text>${this.state.currentUserID}</Text></View> */}
     </React.Fragment>
     );
   }
---
+++ client/components/landing components/Landing.js
@@ -0,0 +1,54 @@
+import React, {Component} from 'react';
+import {StyleSheet, View, Text,  TouchableOpacity, ImageBackground} from 'react-native';
+import Login from "./Login.js";
+import { withTheme } from 'react-native-elements';
+
+
+export default class Landing extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            viewSection :false,
+        }
+    }
+    renderBottomComponent() {
+        if(this.state.viewSection) {
+          return (
+            <Login style={styleschat.form} users={this.props.users} changeUser={this.props.changeUser} addUser={this.props.addUser}/>
+          )
+        }
+      }
+    buttonPressLanding=()=>{
+        this.setState({viewSection:true})
+    }
+    render() {
+        return (
+            <React.Fragment>
+                <ImageBackground source={this.props.background} style={styleschat.container}>
+                    <TouchableOpacity onPress={this.buttonPressLanding}>
+                    <Text style={styleschat.text}>MeetUp</Text>
+                    </TouchableOpacity>
+                    {this.renderBottomComponent()}
+                </ImageBackground>
+                
+            </React.Fragment>
+      );
+    }
+  }
+  const styleschat = StyleSheet.create({
+    container: {
+      flex: 1,
+      left: 0,
+      alignItems: 'center',
+      justifyContent: 'center',
+      width: '100%',
+      height: '100%'
+    },
+    text: {
+        textAlign: 'center',
+        fontSize: 45,
+        color: 'white',
+        marginBottom: 30,
+        width: "90%"
+    },
+  });
