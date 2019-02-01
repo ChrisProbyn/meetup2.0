eac18852f05ac7edd50cba333f6b6c74c2c557f0
Brian <wubrian1234@hotmail.com>
2019-02-01 14:19:24 -0800

    edit chat

--- client/components/chat.js
+++ client/components/chat.js
@@ -1,117 +1,9 @@
 import React, {Component} from 'react';
 import { GiftedChat } from 'react-native-gifted-chat';
-import { AsyncStorage, Text, StyleSheet, View, ActivityIndicator } from 'react-native'
 import gql from "graphql-tag";
 import { Mutation } from "react-apollo";
 
-export default class Chat extends Component {
-  state = {
-    user: 6000,
-    messages: []
-  }
-
-  async componentDidMount() {
-    this._subscribeToNewMessages()
-    await this._initUser()
-  }
-
-  sendComplete(data) {
-    this.setState(previousState => ({
-      messages: GiftedChat.append(previousState.messages, data.createMessage.text),
-    }))
-  }
-
-  render() {
-    if (!this.state.user) {
-      return (
-        <View style={styles.container}>
-          <Text>No user found.</Text>
-        </View>
-      )
-    }
-
-    if (this.props.allMessagesQuery.loading) {
-      return (
-        <View style={styles.container}>
-          <Text>Loading messages ...</Text>
-          <ActivityIndicator style={styles.loadingIndicator} />
-        </View>
-      )
-    }
-    const messages = this.props.allMessagesQuery.messages ? this.props.allMessagesQuery.messages.map(this._convertMessageToGiftedChatFormat) : [] 
-    const userID = this.props.navigation.getParam("userID")
-    const groupName = this.props.navigation.getParam("groupName")
-    console.log(`Render messages: ${JSON.stringify(messages)}`)
-    return (
-      <Mutation mutation={createMessage} onCompleted={this.sendComplete}>
-        {(createMessage, {data}) => (
-        <GiftedChat 
-          messages={messages}
-          onSend={this._onSend}
-          user={"Bob"}
-          // onSend={messages => createMessage({variables:{userID: userID, Group_name: groupName, text: messages[0].text}})}
-          />
-        )}
-      </Mutation>
-    )
-  }
-
-_onSend = (messages) => {
-  // const message = messages[0]
-  // const variables = { text: message.text, sentById: message.user._id }
-  this.props.createMessageMutation({variables:{userID: userID, Group_name: groupName, text: messages[0].text}})
-}
-_subscribeToNewMessages = () => {
-  this.createMessageSubscription = this.props.allMessagesQuery.subscribeToMore({
-    document: newMessageSubscription,
-    updateQuery: (previousState, {subscriptionData}) => {
-      const newMessage = subscriptionData.data.Message.node
-      const messages = [newMessage].concat(previousState.allMessages)
-      return {
-        allMessages: messages
-      }
-    },
-    onError: (err) => console.error(err),
-  })
-}
-
-_convertMessageToGiftedChatFormat = (message) => {
-  console.log(message)
-  return {
-    _id: message.id,
-    user: {
-      _id: message.user.id,
-      name: message.user.username
-    },
-    text: message.text,
-    createdAt: message.created_at
-  }
-}
-
-}
-
-// const allMessages = gql`
-//   query messages {
-//     messages() {
-//       id
-//       text
-//       created_at
-//     }
-//   }
-// `
-
-// const newMessageSubscription = gql`
-//   subscription Message(filter: {
-//       mutation_in: [CREATED]
-//     }) {
-//       node {
-//         id
-//         text
-//         created_at
-//       }
-//   }
-// `
-const createMessage = gql`
+const SEND_MESSAGE_MUTATION = gql`
    mutation CreateMessage($userid: Int, $Group_name: String,  $text: String) {
     createMessage(userid: $userid, Group_name: $Group_name,  text: $text) {
      id
@@ -119,4 +11,71 @@ const createMessage = gql`
      created_at
     }
   }
-`
+`;
+
+const MESSAGE_SENT_SUBSCRIPTION = gql`
+  subscription MessageAdded($groupIds: ID!){
+    messageAdded(groupIds: $groupIds){
+      id
+      text
+      created_at
+    }
+  }
+`;
+
+const CHATS_QUERY = gql`
+  query messages {
+    Message {
+      id
+      text
+      created_at
+    }
+  }
+`;
+
+
+export default class Chat extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {messages: []};
+  }
+
+  componentWillMount() {
+    this.setState({
+      messages: [
+        {
+          _id: 1,
+          text: 'Hello developer',
+          createdAt: new Date(Date.UTC(2016, 7, 30, 17, 20, 0)),
+          user: {
+            _id: 2,
+            name: 'React Native',
+            avatar: 'https://facebook.github.io/react/img/logo_og.png',
+          },
+        },
+      ],
+    });
+  }
+  onSend(messages = []) {
+    this.setState((previousState) => {
+      return {
+        messages: GiftedChat.append(previousState.messages, messages),
+      };
+    });
+  }
+ 
+  render() {
+    const userID = this.props.navigation.getParam("userID")
+    const groupName = this.props.navigation.getParam("groupName")
+    return (
+      <Mutation mutation={SEND_MESSAGE_MUTATION} onCompleted={this.sendComplete}>
+        {(createMessage, {data}) => (
+          <GiftedChat
+          messages={this.state.messages}
+          onSend={this.onSend}
+        />
+        )}
+      </Mutation>
+    )
+  }
+}
