b6a01c1261971bcd220226a7dba7daf9c75eeb54
Brian <wubrian1234@hotmail.com>
2019-02-03 16:00:03 -0800

    map places and center location

--- client/components/map.js
+++ client/components/map.js
@@ -1,9 +1,10 @@
 import React, {Component} from 'react';
 import MapView, {PROVIDER_GOOGLE, Polyline, ProviderPropType} from 'react-native-maps';
-import {   StyleSheet, View, Text, Dimensions, TouchableOpacity, } from 'react-native';
+import { View, Text } from 'react-native';
 import { Query } from 'react-apollo';
 import gql from 'graphql-tag';
 import mapStyle from "./mapstyle.js"
+
 
 
 const flagImage = require('../assets/flag-icon.png')
@@ -11,10 +12,42 @@ const flagImage = require('../assets/flag-icon.png')
 export default class Map extends Component {
     constructor(props){
         super(props);
+        this.state ={ havePlaces: false}
     }
+
+    componentDidMount(){
+        return fetch('https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=49.2790,-123.1187&radius=150&type=restaurant&key=AIzaSyCA6u5xPFKnTN-iyVtUYWsdR9xWOMec14M')
+          .then((response) => response.json())
+          .then((responseJson) => {
+            this.setState({
+                havePlaces: true,
+                dataSource: responseJson.results,
+              });
+          })
+          .catch((error) =>{
+            console.error(error);
+          });
+      }
 
     randomColor() {
         return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    }
+
+    renderPlacesMarker() {
+        if(this.state.havePlaces){
+           return this.state.dataSource.map((place) => {
+                return <MapView.Marker
+                key={place.id} 
+                coordinate={{
+                    latitude: place.geometry.location.lat,
+                    longitude: place.geometry.location.lng}}
+                title={place.name}
+                image={place.icon}
+                pinColor={"red"}
+                description={`rating: ${place.rating}/5, types: ${place.types[0]}`}
+                />
+            })
+        }
     }
 
     render() {
@@ -49,6 +82,15 @@ export default class Map extends Component {
           }, {latitude: 0, longitude: 0});
         };
 
+        coordArray = (array) => {
+            const array2 = array.map((user) => {
+                return {latitude: user.location.lat, longitude: user.location.long}
+            })
+            let arrayItemOne = array2[0];
+            array2.push(arrayItemOne);
+            return array2;
+        }
+
       return (
     <Query query={query}>
     {({loading, error, data}) => {
@@ -59,6 +101,8 @@ export default class Map extends Component {
           return {latitude: user.location.lat, longitude: user.location.long}
         })
         centroid = Center(positions);
+        console.log(centroid)
+        
         return (
           <MapView
             customMapStyle={mapStyle}
@@ -74,16 +118,11 @@ export default class Map extends Component {
             followsUserLocation = {true}
           >
 
-<Polyline
-        coordinates={[
-            { latitude: 49.2813, longitude: -123.1145 },
-            { latitude: 49.2844, longitude: -123.1089 },
-            { latitude: 49.2757, longitude: -123.1199 },
-            { latitude: 49.2813, longitude: -123.1145 },
-        ]}
-        strokeColor="rebeccapurple"
-        strokeWidth={6}
-    />
+        <Polyline
+            coordinates={coordArray(data.group.users)}
+            strokeColor="rebeccapurple"
+            strokeWidth={6}
+        />
 
             {data.group.users.map(marker => (
                 <MapView.Marker 
@@ -101,6 +140,7 @@ export default class Map extends Component {
                 title={"center"}
                 image={flagImage}
                 />
+            {this.renderPlacesMarker()}
           </MapView>
           )
         }
